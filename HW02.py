626d9a33877730e8d781ffca503a3d7afbfb5f63
Fat-AK <102995741+Fat-AK@users.noreply.github.com>
2022-11-03 22:26:21 -0600

    Update HW02.py

--- HW02.py
+++ HW02.py
@@ -5,8 +5,8 @@ from numpy import random
 x = random.uniform(low=0.0, high=1.0, size=100) # random numbers as input values 
 t = np.empty(100) #  target array 
 
-i = np.arange (0,100) # define index 
-t[i]=x[i]**3-x[i]**2 # target values 
+for i in range (0,100):
+    t[i]=x[i]**3-x[i]**2 # target values 
 
 import matplotlib.pyplot as plt
 
