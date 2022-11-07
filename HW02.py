b0ed3a4ccd0f2be855ae022856d28b090444bba6
Fat-AK <102995741+Fat-AK@users.noreply.github.com>
2022-11-06 20:47:15 -0700

    Update HW02.py

--- HW02.py
+++ HW02.py
@@ -17,3 +17,31 @@ import matplotlib.pyplot as plt
 plt.figure(figsize=(12,5))
 plt.plot (t,x)
 plt.show()
+
+# Task02
+
+def threshold (x):
+    if x> 0:
+        return 1
+    else:
+        return 0
+
+class Perceptron():
+    def __init__(self, n_units, input_units, biasvector, weightmatrix,threshold_function = threshold):  
+        
+        self. n_units = n_units
+        self. input_units = input_units
+        self. biasvector = biasvector 
+        self. weightmatrix = weight
+        self. threshold_function = threshold_function
+        
+    def __call__ (self, input_units:random.uniform(low=0.0, high=1.0, size=10)): 
+        T = self. weightmatrix @ input_units + self. biasvector
+        return self.threshold_function(T)
+    
+weight = random.uniform(low=1, high=2, size=10)
+bias = np.zeros (10)
+
+input_units = random.uniform(low=0.0, high=1.0, size=10)
+
+perceptron01 = Perceptron (10, input_units, weight, bias)
