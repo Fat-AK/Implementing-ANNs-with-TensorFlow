66bb54e36328c641f4f88b9f82a6db85ba9215a4
Katarzyna <87017109+Dakini87@users.noreply.github.com>
2022-11-13 15:24:14 +0100

    Update Hmw_3.py
    '' correction q1

--- Hmw_3.py
+++ Hmw_3.py
@@ -2,8 +2,7 @@
 import tensorflow_datasets as tfds 
 import tensorflow as tf
 
-(train_ds, test_ds), ds_info = tfds.load(’mnist’, split=[’train’, ’
-test’], as_supervised=True, with_info=True)
+(train_ds, test_ds), ds_info = tfds.load('mnist', split=['train', 'test'], as_supervised=True, with_info=True)
 
 #Questions
 #How many training/test images are there? 
