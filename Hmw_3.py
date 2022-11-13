7c148bc58bf025175a35a97857886ab3960228ca
Katarzyna <87017109+Dakini87@users.noreply.github.com>
2022-11-13 15:58:58 +0100

    Update Hmw_3.py
    q 2.4

--- Hmw_3.py
+++ Hmw_3.py
@@ -18,4 +18,117 @@ tfds.show_examples(train_ds , ds_info)
 print(ds_info)
 
 #Task 2.2
+def prepare_mnist_data(mnist):
+  #flatten into the vectors
+  mnist = mnist.map(lambda img, target: (tf.reshape(img, (-1,)), target))
+  #convert data 
+  mnist = mnist.map(lambda img, target: (tf.cast(img, tf.float32), target))
+  #sloppy input normalization
+  mnist = mnist.map(lambda img, target: ((img/128.)-1., target))
+  #create one-hot targets
+  mnist = mnist.map(lambda img, target: (img, tf.one_hot(target, depth=10)))
+  #cache this progress in memory
+  mnist = mnist.cache()
+  #shuffle, batch, prefetch
+  mnist = mnist.shuffle(1000)
+  mnist = mnist.batch(32)
+  mnist = mnist.prefetch(20)
+  #return preprocessed dataset
+  return mnist
 
+train_dataset = train_ds.apply(prepare_mnist_data)
+test_dataset = test_ds.apply(prepare_mnist_data)
+
+#Task 2.3
+from tensorflow.keras.layers import Dense
+
+class MyModel(tf.keras.Model):
+    
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.dense1 = tf.keras.layers.Dense(256, activation=tf.nn.relu)
+        self.dense2 = tf.keras.layers.Dense(256, activation=tf.nn.relu)
+        self.out = tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+
+    @tf.function
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        x = self.dense2(x)
+        x = self.out(x)
+        return x
+      
+#Task 2.4
+def train_step(model, input, target, loss_function, optimizer):
+  
+  with tf.GradientTape() as tape:
+    prediction = model(input)
+    loss = loss_function(target, prediction)
+  gradients = tape.gradient(loss, model.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+  return loss
+
+def test(model, test_data, loss_function):
+  # test complete test data
+
+  test_accuracy_aggregator = []
+  test_loss_aggregator = []
+
+  for (input, target) in test_data:
+    prediction = model(input)
+    sample_test_loss = loss_function(target, prediction)
+    sample_test_accuracy =  np.argmax(target, axis=1) == np.argmax(prediction, axis=1)
+    sample_test_accuracy = np.mean(sample_test_accuracy)
+    test_loss_aggregator.append(sample_test_loss.numpy())
+    test_accuracy_aggregator.append(np.mean(sample_test_accuracy))
+
+  test_loss = tf.reduce_mean(test_loss_aggregator)
+  test_accuracy = tf.reduce_mean(test_accuracy_aggregator)
+
+  return test_loss, test_accuracy
+
+tf.keras.backend.clear_session()
+
+#For showcasing we only use a subset of the training and test data (generally use all of the available data!)
+train_dataset = train_dataset.take(1000)
+test_dataset = test_dataset.take(100)
+
+### Hyperparameters
+num_epochs = 10
+learning_rate = 0.1
+
+# Initialize the model.
+model = MyModel()
+cross_entropy_loss = tf.keras.losses.CategoricalCrossentropy()
+optimizer = tf.keras.optimizers.SGD(learning_rate)
+
+# Initialize lists for visualization.
+train_losses = []
+test_losses = []
+test_accuracies = []
+
+#testing 
+test_loss, test_accuracy = test(model, test_dataset, cross_entropy_loss)
+test_losses.append(test_loss)
+test_accuracies.append(test_accuracy)
+
+#check model performence 
+train_loss, _ = test(model, train_dataset, cross_entropy_loss)
+train_losses.append(train_loss)
+
+# We train for num_epochs epochs.
+for epoch in range(num_epochs):
+    print(f'Epoch: {str(epoch)} starting with accuracy {test_accuracies[-1]}')
+
+    #training 
+    epoch_loss_agg = []
+    for input,target in train_dataset:
+        train_loss = train_step(model, input, target, cross_entropy_loss, optimizer)
+        epoch_loss_agg.append(train_loss)
+    
+    #track training loss
+    train_losses.append(tf.reduce_mean(epoch_loss_agg))
+
+    #testin
+    test_loss, test_accuracy = test(model, test_dataset, cross_entropy_loss)
+    test_losses.append(test_loss)
+    test_accuracies.append(test_accuracy)
