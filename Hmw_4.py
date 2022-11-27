3673e9a027c743fdde42e9040f7770420e837884
Katarzyna <87017109+Dakini87@users.noreply.github.com>
2022-11-27 17:36:42 +0100

    Update Hmw_4.py

--- Hmw_4.py
+++ Hmw_4.py
@@ -1,109 +1,126 @@
-#Task 2
-
+import tensorflow_datasets as tfds
 import tensorflow as tf
 
-# Import TensorFlow Datasets
-import tensorflow_datasets as tfds
-tfds.disable_progress_bar()
+(train_ds, test_ds), ds_info = tfds.load('mnist', split=['train', 'test'], as_supervised=True, with_info=True)
 
-# Helper libraries
-import math
-import numpy as np
-import matplotlib.pyplot as plt
-
-import logging
-logger = tf.get_logger()
-logger.setLevel(logging.ERROR)
-
-dataset, metadata = tfds.load('mnist', as_supervised=True, with_info=True)
-train_dataset, test_dataset = dataset['train'], dataset['test']
-
-class_names = metadata.features['label'].names
-print("Class names: {}".format(class_names))
-
-num_train_examples = metadata.splits['train'].num_examples
-num_test_examples = metadata.splits['test'].num_examples
-print("Number of training examples: {}".format(num_train_examples))
-print("Number of test examples:     {}".format(num_test_examples))
-
-def normalize(images, labels):
-  images = tf.cast(images, tf.float32)
-  images /= 255
-  return images, labels
-
-# The map function applies the normalize function to each element in the train
-# and test datasets
-train_dataset =  train_dataset.map(normalize)
-test_dataset  =  test_dataset.map(normalize)
-
-# The first time you use the dataset, the images will be loaded from disk
-# Caching will keep them in memory, making training faster
-train_dataset =  train_dataset.cache()
-test_dataset  =  test_dataset.cache()
-
-# Take a single image, and remove the color dimension by reshaping
-for image, label in test_dataset.take(1):
-  break
-image = image.numpy().reshape((28,28))
-
-# Plot the image - voila a piece of fashion clothing
-plt.figure()
-plt.imshow(image, cmap=plt.cm.binary)
-plt.colorbar()
-plt.grid(False)
-plt.show()
-
-
-plt.figure(figsize=(10,10))
-for i, (image, label) in enumerate(train_dataset.take(25)):
-    image = image.numpy().reshape((28,28))
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(image, cmap=plt.cm.binary)
-    plt.xlabel(class_names[label])
-plt.show()
-
-model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28, 1)),tf.keras.layers.Dense(128, activation=tf.nn.relu
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-              metrics=['accuracy'])
-BATCH_SIZE = 32
-train_dataset = train_dataset.cache().repeat().shuffle(num_train_examples).batch(BATCH_SIZE)
-test_dataset = test_dataset.cache().batch(BATCH_SIZE)
-                                                                                                    
-                                                                                                    
-model.fit(train_dataset, epochs=5, steps_per_epoch=math.ceil(num_train_examples/BATCH_SIZE))
-                                                                                                    
-test_loss, test_accuracy = model.evaluate(test_dataset, steps=math.ceil(num_test_examples/32))
-print('Accuracy on test dataset:', test_accuracy)
-                                                                                                   
-for test_images, test_labels in test_dataset.take(1):
-  test_images = test_images.numpy()
-  test_labels = test_labels.numpy()
-
-  def sub1():
-      for a in test_labels:
-          if tf . add (a,a+1) >= 5:
-            print (a, a+1)
  
-sub1()
-                                                                                                    
-for test_images, test_labels in test_dataset.take(1):
-  test_images = test_images.numpy()
-  test_labels = test_labels.numpy()
-  
-def sub2():
-  for a in test_labels:
-    y = a - (a+1)
-    predictions = model.predict(y)
 
-print (predictions)
-                                                                                                                                                          
-                                                                                                    
-                                                                                                    
-                                                                                             
-                                                                                                    
-                                                                                                  
-                                                                                                    
+def preprocessing_fn(ds, condition):
+
+  if condition == 'subtraction':
+
+    pass
+
+  elif condition == 'larger_than_five':
+
+    pass
+
+  else:
+
+    raise ValueError('Condition must be either "subtraction" or "larger_than_five".')
+
+    
+
+    
+
+def train_fn(subtask, optimizer):
+
+  if subtask not in ['subtraction', 'larger_than_five']:
+
+    raise ValueError('Subtask must be either "subtraction" or "larger_than_five".')
+
+    
+
+  ds = preprocessing_fn(train_ds, subtask)
+
+  
+
+  if subtask == 'subtraction':
+
+    model = tf.keras.Sequential([
+
+      tf.keras.layers.Input(shape=(28, 28, 1)),
+
+      tf.keras.layers.Conv2D(32, 3, activation='relu'),
+
+      tf.keras.layers.Flatten(),
+
+      tf.keras.layers.Dense(10, activation='softmax')
+
+    ])
+
+    
+
+    model.compile(
+
+      optimizer=optimizer,
+
+      loss='sparse_categorical_crossentropy',
+
+      metrics=['accuracy']
+
+    )
+
+    
+
+    model.fit(
+
+      ds,
+
+      epochs=5
+
+    )
+
+    
+
+  elif subtask == 'larger_than_five':
+
+    model = tf.keras.Sequential([
+
+      tf.keras.layers.Input(shape=(28, 28, 1)),
+
+      tf.keras.layers.Conv2D(32, 3, activation='relu'),
+
+      tf.keras.layers.Flatten(),
+
+      tf.keras.layers.Dense(1, activation='sigmoid')
+
+    ])
+
+    
+
+    model.compile(
+
+      optimizer=optimizer,
+
+      loss='binary_crossentropy',
+
+      metrics=['accuracy']
+
+    )
+
+    
+
+    model.fit(
+
+      ds,
+
+      epochs=5
+
+    )
+
+    
+
+train_fn('subtraction', tf.keras.optimizers.Adam())
+
+train_fn('larger_than_five', tf.keras.optimizers.Adam()) 
+
+#The first section imports the necessary packages for loading the MNIST dataset and building a neural network.
+
+#The second section defines a preprocessing function that can be used to format the data for either of the two subtasks. This function takes as input a dataset and a condition, and outputs a dataset that is formatted according to the specified condition.
+
+# The third section defines a train function that can be used to train a neural network for either of the two subtasks. This function takes as input a subtask and an optimizer, and outputs a trained neural network.
+
+#The fourth section uses the train function to train a neural network for the subtask of subtracting two MNIST digits.
+
+# The fifth section uses the train function to train a neural network for the subtask of determining whether the sum of two MNIST digits is greater than five.
