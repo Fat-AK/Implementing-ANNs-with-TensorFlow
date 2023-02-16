20567cb760d73580a1fd44b8995db474379508bd
Katarzyna <87017109+Dakini87@users.noreply.github.com>
2023-02-16 20:33:10 +0100

    Update Hmw_11.py

--- Hmw_11.py
+++ Hmw_11.py
@@ -72,16 +72,86 @@ class TransformerBlock(tf.keras.layers.Layer):
         result = self.batchnorm2(x)
         return result
 
+# define the model
 class Model(tf.keras.Model):
-    def __init__(self):
+    def __init__(self, vocab_size, embedding_size, num_transformer_blocks):
         super().__init__()
-        self.tokenizer = tokenizer
-        self.optimizer = tf.keras.optimizers.Adam()
-        self.loss_function = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        self.token_embedding = tf.keras.layers.Embedding(vocab_size, embedding_size)
+        self.transformer_blocks = [TransformerBlock() for _ in range(num_transformer_blocks)]
+        self.dense = tf.keras.layers.Dense(vocab_size)
 
-        # define the metrics
-        self.loss_metric = tf.keras.metrics.Mean(name="loss")
-        self.acc_metric = tf.keras.metrics.SparseCategoricalAccuracy(name="acc")
-        self.top_k_acc_metric = tf.keras.metrics.SparseTopKCategoricalAccuracy(10, name="top_10_acc")
+    def call(self, input, training):
+        embedded = self.token_embedding(input)
+        for block in self.transformer_blocks:
+            embedded = block(embedded, training=training)
+        logits = self.dense(embedded)
+        return logits
 
-       
+# define hyperparameters
+vocab_size = 4096
+embedding_size = 128
+num_transformer_blocks = 2
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 10
+
+# create the model
+model = Model(vocab_size, embedding_size, num_transformer_blocks)
+
+# define the optimizer and loss function
+optimizer = tf.keras.optimizers.Adam(learning_rate)
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+# define the metrics
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_acc = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+val_loss = tf.keras.metrics.Mean(name='val_loss')
+val_acc = tf.keras.metrics.SparseCategoricalAccuracy(name='val_accuracy')
+
+# define the file writers for TensorBoard
+train_log_dir = 'logs/train'
+train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+val_log_dir = 'logs/val'
+val_summary_writer = tf.summary.create_file_writer(val_log_dir)
+
+# define the training loop
+@tf.function
+def train_step(inputs, targets):
+    with tf.GradientTape() as tape:
+        logits = model(inputs, training=True)
+        loss = loss_fn(targets, logits)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    train_loss(loss)
+    train_acc(targets, logits)
+
+@tf.function
+def val_step(inputs, targets):
+    logits = model(inputs, training=False)
+    loss = loss_fn(targets, logits)
+    val_loss(loss)
+    val_acc(targets, logits)
+
+# define the dataset
+train_dataset = tf.data.Dataset.from_tensor_slices((inputs_train, targets_train)).shuffle(buffer_size).batch(batch_size, drop_remainder=True)
+val_dataset = tf.data.Dataset.from_tensor_slices((inputs_val, targets_val)).batch(batch_size, drop_remainder=True)
+
+# define the training loop
+for epoch in range(num_epochs):
+    print(f'Epoch {epoch+1}/{num_epochs}')
+    for step, (inputs, targets) in enumerate(train_dataset):
+        train_step(inputs, targets)
+        if step % 100 == 0:
+            print(f'Step {step}, Loss: {train_loss.result()}, Accuracy: {train_acc.result()}')
+    with train_summary_writer.as_default():
+        tf.summary.scalar('loss', train_loss.result(), step=epoch)
+        tf.summary.scalar('accuracy', train_acc.result(), step=epoch)
+    for inputs, targets in val_dataset:
+        val_step(inputs, targets)
+    with val_summary_writer.as_default():
+        tf.summary.scalar('loss', val_loss.result(), step=epoch)
+        tf.summary.scalar('accuracy', val_acc.result(), step=epoch)
+    print(f'Validation Loss: {val_loss.result()}, Validation Accuracy: {val_acc.result()}')
+
+# save the model
+model.save('model.h5')
